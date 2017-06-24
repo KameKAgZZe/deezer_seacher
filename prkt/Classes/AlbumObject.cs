773f1fcf2baa57fe9c753278e1d21c8d8b22700a
KameKAgZZe <kamekagzze@mail.ru>
2017-06-24 12:09:14 +0600

    hello

---
+++ prkt/Classes/AlbumObject.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections.Generic;
+
+public class Datum
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string picture { get; set; }
+    public string type { get; set; }
+}
+
+public class Genres
+{
+    public List<Datum> data { get; set; }
+}
+
+public class Contributor
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string link { get; set; }
+    public string share { get; set; }
+    public string picture { get; set; }
+    public string picture_small { get; set; }
+    public string picture_medium { get; set; }
+    public string picture_big { get; set; }
+    public string picture_xl { get; set; }
+    public bool radio { get; set; }
+    public string tracklist { get; set; }
+    public string type { get; set; }
+    public string role { get; set; }
+}
+
+public class Artist
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string picture { get; set; }
+    public string picture_small { get; set; }
+    public string picture_medium { get; set; }
+    public string picture_big { get; set; }
+    public string picture_xl { get; set; }
+    public string tracklist { get; set; }
+    public string type { get; set; }
+}
+
+public class Artist2
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string tracklist { get; set; }
+    public string type { get; set; }
+}
+
+public class Artist3
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string tracklist { get; set; }
+    public string type { get; set; }
+}
+
+public class Alternative
+{
+    public int id { get; set; }
+    public bool readable { get; set; }
+    public string title { get; set; }
+    public string title_short { get; set; }
+    public string title_version { get; set; }
+    public string link { get; set; }
+    public int duration { get; set; }
+    public int rank { get; set; }
+    public bool explicit_lyrics { get; set; }
+    public string preview { get; set; }
+    public Artist3 artist { get; set; }
+    public string type { get; set; }
+}
+
+public class Datum2
+{
+    public int id { get; set; }
+    public bool readable { get; set; }
+    public string title { get; set; }
+    public string title_short { get; set; }
+    public string title_version { get; set; }
+    public string link { get; set; }
+    public int duration { get; set; }
+    public int rank { get; set; }
+    public bool explicit_lyrics { get; set; }
+    public string preview { get; set; }
+    public Artist2 artist { get; set; }
+    public string type { get; set; }
+    public Alternative alternative { get; set; }
+}
+
+public class Tracks
+{
+    public List<Datum2> data { get; set; }
+}
+
+public class AlbumObject
+{
+    public int id { get; set; }
+    public string title { get; set; }
+    public string upc { get; set; }
+    public string link { get; set; }
+    public string share { get; set; }
+    public string cover { get; set; }
+    public string cover_small { get; set; }
+    public string cover_medium { get; set; }
+    public string cover_big { get; set; }
+    public string cover_xl { get; set; }
+    public int genre_id { get; set; }
+    public Genres genres { get; set; }
+    public string label { get; set; }
+    public int nb_tracks { get; set; }
+    public int duration { get; set; }
+    public int fans { get; set; }
+    public int rating { get; set; }
+    public string release_date { get; set; }
+    public string record_type { get; set; }
+    public bool available { get; set; }
+    public string tracklist { get; set; }
+    public bool explicit_lyrics { get; set; }
+    public List<Contributor> contributors { get; set; }
+    public Artist artist { get; set; }
+    public string type { get; set; }
+    public Tracks tracks { get; set; }
+}
--- prkt/Controllers/HomeController.cs
+++ prkt/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using prkt.Models;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using prkt.Classes;
+using prkt.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +9,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Xml.Linq;
 using System.Xml.Serialization;
+using unirest_net.http;
 
 namespace prkt.Controllers
 {
@@ -14,15 +18,39 @@ namespace prkt.Controllers
 
         [HttpGet]
         public ActionResult Index()
-        { 
-            
-                XDocument xdoc;
-                xdoc = XDocument.Load("http://msiter.ru/sites/default/files/msiter_files/cd_catalog.xml");
-                var q = FromXml(xdoc);
-                return View(q);
+        {
+
+            //XDocument xdoc;
+            //xdoc = XDocument.Load("http://msiter.ru/sites/default/files/msiter_files/cd_catalog.xml");
+
+            //var q = FromXml(xdoc);
+            //return View(q);
+            return View();
 
         }
-
+        public ActionResult DeezerSearch(string nick)
+        {
+            HttpResponse<string> response = Unirest.get("https://deezerdevs-deezer.p.mashape.com/search?q="+ nick)
+.header("X-Mashape-Key", "ejWTgwZXAlmshfC6wOWKifd9Am2Lp1OrhYkjsnfOF0oNRh1L9f")
+.header("Accept", "text/plain")
+.asString();
+            var JsonQuote = JsonConvert.DeserializeObject<TrackObject>(response.Body);
+            ViewBag.response =JsonQuote.data;
+            ViewBag.total = JsonQuote.total;
+           return PartialView("result");
+        }
+        public ActionResult AlbumSearch(int id)
+        {
+            HttpResponse<string> response = Unirest.get("https://deezerdevs-deezer.p.mashape.com/album/"+id)
+.header("X-Mashape-Key", "ejWTgwZXAlmshfC6wOWKifd9Am2Lp1OrhYkjsnfOF0oNRh1L9f")
+.header("Accept", "text/plain")
+.asString();
+            var JsonQuote = JsonConvert.DeserializeObject<AlbumObject>(response.Body);
+            ViewBag.response = JsonQuote;
+            ViewBag.genres = JsonQuote.genres.data;
+            ViewBag.tracks = JsonQuote.tracks.data;
+            return PartialView("albumsearch");
+        }
         private Models.CATALOG FromXml(XDocument xd)
         {
 
